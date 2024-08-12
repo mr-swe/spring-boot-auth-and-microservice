13f8cddaf8205e491ab34540741bcedd128e201a
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-08-12 23:38:55 +0600

    registered to eureka

---
+++ family_care/src/test/java/com/example/family_care/FamilyCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.family_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- family_transaction/src/main/java/com/example/family_transaction/FamilyTransactionApplication.java
+++ family_transaction/src/main/java/com/example/family_transaction/FamilyTransactionApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FamilyTransactionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FamilyTransactionApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyTransactionApplication.class, args);
+    }
 
 }
