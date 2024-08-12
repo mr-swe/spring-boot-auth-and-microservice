52867bae70eaf38d5d1b92339a887843b7511d0d
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-08-12 22:07:51 +0600

    first commit

---
+++ family_transaction/src/main/java/com/example/family_transaction/FamilyTransactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.family_transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyTransactionApplication.class, args);
+	}
+
+}
