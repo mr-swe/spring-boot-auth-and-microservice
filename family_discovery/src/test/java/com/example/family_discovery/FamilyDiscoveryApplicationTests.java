d41c5cbb6a0ae3616cdc79dc57b9284bf45d5771
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-08-13 17:13:59 +0600

    ui project created with angular

---
+++ family_discovery/src/test/java/com/example/family_discovery/FamilyDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.family_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
