72349cfcd47345eaafee3700ba11e467d8138c48
Mahfuz <mahfuzur.rahman.swe@gmail.com>
2024-08-21 16:44:46 +0600

    authorization server implemented separately

---
+++ family-authorization/src/test/java/com/example/family_authorization/FamilyAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.family_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
